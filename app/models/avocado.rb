f1005254dbac69b5aa54cfb4a1e5d26cad2bf5b8
NateBigStone <n.gaut@bigstonemgmt.com>
2017-11-13 22:04:02 -0600

    gmap

--- app/models/avocado.rb
+++ app/models/avocado.rb
@@ -3,19 +3,18 @@ class Avocado < ApplicationRecord
   belongs_to :user
   belongs_to :location
   enum category: [ "Tiny Hass",  "Small Hass", "Medium Hass", "Large Hass", "Jumbo Hass", "Other Kind" ]
-  # def tiny_unit
-  #   3.1 #ounces
-  # end
-  # def small_unit
-  #   5.2 #ounces
-  # end
-  # def medium_unit
-  #   7.4 #ounces
-  # end
-  # def large_unit
-  #   9.8 #ounces
-  # end
-  # def jumbo_unit
-  #   12.2 #ounces
-  # end
+  def image_math(level)
+    level = level.to_i
+    image = ""
+    if level == 10
+      image = "10ripe"
+    elsif level > 7
+      image = "9ripe"
+    elsif level > 6
+      image = "7ripe"
+    else
+      image = "1ripe"
+    end
+    image
+  end
 end
